ae077ad9fc76468b56a67535cd4527e8b27bae88
Jacopo Simoni <simonij@tcd.ie>
2025-01-30 16:56:10 -0600

    (1) replaced ; with , in sdm_oriented return std::array (2) created a new function to compute atom localized magnetic moments

--- src/observables/magnetization.hpp
+++ src/observables/magnetization.hpp
@@ -44,10 +44,10 @@ auto sdm_oriented(vector3<double> const & mp) {
 	assert(norm(mp) <= 1.0+tol);
 
 	return std::array<double, 4>{{
-		/*[0] =*/ (1.0 + mp[2])/2.0;
-		/*[1] =*/ (1.0 - mp[2])/2.0;
-		/*[2] =*/      + mp[0] /2.0;
-		/*[3] =*/      - mp[1] /2.0;
+		/*[0] =*/ (1.0 + mp[2])/2.0,
+		/*[1] =*/ (1.0 - mp[2])/2.0,
+		/*[2] =*/      + mp[0] /2.0,
+		/*[3] =*/      - mp[1] /2.0
 	}};
 }
 
@@ -72,6 +72,25 @@ auto total_magnetization(basis::field_set<basis::real_space, double> const & spi
 		return operations::integral(observables::magnetization(spin_density));
 	} else {
 		return vector3<double>{0.0, 0.0, 0.0};
+	}
+}
+
+void local_magnetic_moments(basis::field_set<basis::real_space, double> const & spin_density, std::vector<double> const & magnetic_radii, std::vector<vector3<double>> const & magnetic_centers, std::vector<vector3<double>> & magnetic_moments) {
+	auto nspin = spin_density.set_size();
+	basis::field<basis::real_space, vector3<double>> local_mag_density(spin_density.basis());
+	for (auto i = 0; i < magnetic_moments.size(); i++) {
+		std::cout << "i: " << i << std::endl;
+		local_mag_density.fill(vector3<double>{0.0, 0.0, 0.0});
+		gpu::run(spin_density.local_set_size(), spin_density.basis().local_sizes()[2], spin_density.basis().local_sizes()[1], spin_density.basis().local_sizes()[0],
+		[spd = begin(spin_density.hypercubic()), magd = begin(local_mag_density.cubic()), point_op = spin_density.basis().point_op(), mr = magnetic_radii[i], mcs = magnetic_centers, mc = magnetic_centers[i], nspin] GPU_LAMBDA (auto ist, auto iz, auto iy, auto ix){
+			auto rr = point_op.rvector_cartesian(ix, iy, iz);
+			auto dd = sqrt(norm(rr - mc));
+			//for (auto i = 0; i < mcs.size(); i++) {
+
+			//}
+			if (dd <= mr) magd[ix][iy][iz] = local_magnetization(spd[ix][iy][iz], nspin);
+		});
+		magnetic_moments[i] = operations::integral(local_mag_density);
 	}
 }
 
