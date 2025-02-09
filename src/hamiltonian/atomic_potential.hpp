57ff711af80d3aeaaf60a58fa8536188fa6d70f6
Jacopo Simoni <simonij@tcd.ie>
2025-02-09 00:28:55 -0600

    fix density initialization in test unit for spin polarized case

--- src/hamiltonian/atomic_potential.hpp
+++ src/hamiltonian/atomic_potential.hpp
@@ -482,10 +482,10 @@ TEST_CASE(INQ_TEST_FILE, INQ_TEST_TAG){
 
 		CHECK(nn_pol.set_size() == 2);
 		CHECK(operations::integral_sum(nn_pol) == 29.9562519176_a);
-		CHECK(nn_pol.hypercubic()[5][3][0][0] == Approx(1.2*0.066529473));
-		CHECK(nn_pol.hypercubic()[3][1][0][0] == Approx(1.2*0.0923002217));
-		CHECK(nn_pol.hypercubic()[5][3][0][1] == Approx(0.8*0.066529473));
-		CHECK(nn_pol.hypercubic()[3][1][0][1] == Approx(0.8*0.0923002217));
+		CHECK(nn_pol.hypercubic()[5][3][0][0] == Approx(0.066529473));
+		CHECK(nn_pol.hypercubic()[3][1][0][0] == Approx(0.0923002217));
+		CHECK(nn_pol.hypercubic()[5][3][0][1] == Approx(0.066529473));
+		CHECK(nn_pol.hypercubic()[3][1][0][1] == Approx(0.0923002217));
 		
 		CHECK(pot.has_nlcc());
 		
